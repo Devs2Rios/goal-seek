b9d60b7140060679c17cb21e6fe640ad3f864bd8
eduairet <hola@eduairet.com>
2022-08-30 20:12:19 -0500

    button logic

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -4,24 +4,35 @@ import Button from '../../UI/Button/Button';
 import './CourseInput.css';
 
 const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+  const [enteredValue, setEnteredValue] = useState(''),
+    [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = event => {
-    setEnteredValue(event.target.value);
+    const val = event.target.value;
+    setEnteredValue(val);
+    setIsValid(val.trim().length > 0 && val ? true : false);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
-    props.onAddGoal(enteredValue);
+    if (isValid) {
+      props.onAddGoal(enteredValue);
+    }
+    setEnteredValue('');
+    setIsValid(true);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <div className={`form-control ${isValid ? '' : 'invalid'}`}>
         <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <input
+          type='text'
+          value={enteredValue}
+          onChange={goalInputChangeHandler}
+        />
       </div>
-      <Button type="submit">Add Goal</Button>
+      <Button type='submit'>Add Goal</Button>
     </form>
   );
 };
